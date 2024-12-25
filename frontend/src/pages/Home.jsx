d8b44f0798ecbe120d42ae2fa7f2e06a8c0c47d2
Arpandobariya15 <work.arpan8980@gmail.com>
2024-12-25 23:48:23 +0530

    frontend-done-login

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { handleSuccess } from '../utils';
+
+const Home = () => {
+    const [loggedInUser, setLoggedInUser] = useState('');
+    useEffect(() => {
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+    }, [])
+
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
+        handleSuccess('User Loggedout');
+        setTimeout(() => {
+            navigate('/login');
+        }, 1000)
+    }
+  return (
+    <div>
+        <h1>Welcome{loggedInUser}</h1>
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+
+            </div>
+            <ToastContainer />
+    </div>
+  )
+}
+
+export default Home
